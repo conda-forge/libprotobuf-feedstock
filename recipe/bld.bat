8224d02284f66df5ad3a3155bb0d9055ab007982
Wes McKinney <wesm+git@apache.org>
2019-02-20 09:34:29 -0600

    Also build static libraries on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -26,3 +26,21 @@ nmake check
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
+
+:: Build static libraries and copy manually
+cmake -G "NMake Makefiles" ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -Dprotobuf_WITH_ZLIB=ON ^
+         -Dprotobuf_BUILD_SHARED_LIBS=OFF ^
+         -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
+         ../..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+copy libprotobuf.lib %LIBRARY_LIB%\libprotobuf_static.lib
+copy libprotoc.lib %LIBRARY_LIB%\libprotoc_static.lib
+if errorlevel 1 exit 1
