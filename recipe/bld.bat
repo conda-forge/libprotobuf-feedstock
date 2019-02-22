7ee28411e693d0e4d82fee0264b811a658a44dd3
Wes McKinney <wesm+git@apache.org>
2019-02-22 15:51:23 -0600

    Incorporate strategy from PR #2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,10 @@
 :: Setup directory structure per protobuf's instructions.
 cd cmake
 if errorlevel 1 exit 1
-mkdir build
+
+mkdir build-shared
 if errorlevel 1 exit 1
-cd build
-if errorlevel 1 exit 1
-mkdir release
-if errorlevel 1 exit 1
-cd release
+cd build-shared
 if errorlevel 1 exit 1
 
 :: Configure and install based on protobuf's instructions and other `bld.bat`s.
@@ -24,7 +21,13 @@ nmake
 if errorlevel 1 exit 1
 nmake check
 if errorlevel 1 exit 1
-nmake install
+
+cd ..
+if errorlevel 1 exit 1
+
+mkdir build-static
+if errorlevel 1 exit 1
+cd build-static
 if errorlevel 1 exit 1
 
 :: Build static libraries and copy manually
@@ -32,6 +35,7 @@ cmake -G "NMake Makefiles" ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCMAKE_RELEASE_POSTFIX="_static" ^
          -Dprotobuf_WITH_ZLIB=ON ^
          -Dprotobuf_BUILD_SHARED_LIBS=OFF ^
          -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
@@ -41,6 +45,14 @@ if errorlevel 1 exit 1
 nmake
 if errorlevel 1 exit 1
 
-copy libprotobuf.lib %LIBRARY_LIB%\libprotobuf_static.lib
-copy libprotoc.lib %LIBRARY_LIB%\libprotoc_static.lib
+nmake install
+if errorlevel 1 exit 1
+
+ren "%LIBRARY_BIN%\protoc.exe" protoc_static.exe
+if errorlevel 1 exit 1
+
+@rem now install the shared libraries
+cd ..\build-shared
+if errorlevel 1 exit 1
+nmake install
 if errorlevel 1 exit 1
