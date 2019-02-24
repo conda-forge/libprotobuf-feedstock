243088b1e318a194b000a4732d0ea3e7430553b2
Wes McKinney <wesm@users.noreply.github.com>
2019-02-24 12:46:07 -0500

    Also build static libraries on Windows (#22)
    * Also build static libraries on Windows
    
    * Bump build number
    
    * MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.2.13, and conda-forge-pinning 2019.02.15
    
    * Incorporate strategy from PR #2
    
    * MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.2.14, and conda-forge-pinning 2019.02.21
    
    * Fix cmake invocations

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,10 @@
 :: Setup directory structure per protobuf's instructions.
 cd cmake
 if errorlevel 1 exit 1
-mkdir build
+
+mkdir build-shared
 if errorlevel 1 exit 1
-cd build
-if errorlevel 1 exit 1
-mkdir release
-if errorlevel 1 exit 1
-cd release
+cd build-shared
 if errorlevel 1 exit 1
 
 :: Configure and install based on protobuf's instructions and other `bld.bat`s.
@@ -18,11 +15,44 @@ cmake -G "NMake Makefiles" ^
          -Dprotobuf_WITH_ZLIB=ON ^
          -Dprotobuf_BUILD_SHARED_LIBS=ON ^
          -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
-         ../..
+         ..
 if errorlevel 1 exit 1
 nmake
 if errorlevel 1 exit 1
 nmake check
+if errorlevel 1 exit 1
+
+cd ..
+if errorlevel 1 exit 1
+
+mkdir build-static
+if errorlevel 1 exit 1
+cd build-static
+if errorlevel 1 exit 1
+
+:: Build static libraries and copy manually
+cmake -G "NMake Makefiles" ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCMAKE_RELEASE_POSTFIX="_static" ^
+         -Dprotobuf_WITH_ZLIB=ON ^
+         -Dprotobuf_BUILD_SHARED_LIBS=OFF ^
+         -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
+         ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+ren "%LIBRARY_BIN%\protoc.exe" protoc_static.exe
+if errorlevel 1 exit 1
+
+@rem now install the shared libraries
+cd ..\build-shared
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
