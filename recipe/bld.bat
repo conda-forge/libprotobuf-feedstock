39b54eae73e14b825619af15d05dc9cd3369a34d
Uwe L. Korn <uwe.korn@quantco.com>
2019-10-30 11:17:36 +0100

    Update hashes and use Ninja on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ cd build-shared
 if errorlevel 1 exit 1
 
 :: Configure and install based on protobuf's instructions and other `bld.bat`s.
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -17,9 +17,5 @@ cmake -G "NMake Makefiles" ^
          -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ^
          ..
 if errorlevel 1 exit 1
-nmake
-if errorlevel 1 exit 1
-nmake check
-if errorlevel 1 exit 1
-nmake install
+cmake --build . --target install --config Release
 if errorlevel 1 exit 1
